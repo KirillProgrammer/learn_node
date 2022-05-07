413b49f0f67ca1ef225826a89d84b7eb5cb22fbc
KirillProgrammer <kirillkarpovprogramm@gmail.com>
2022-05-07 18:09:18 +0300

    Работу с пользователями вынес в отдельный сервис

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { App } from "./app"
-import { LoggerService } from "./logger/logger.service";
+import { LoggerService } from "./logger/logger.service"
+import { UserController } from "./users/users.controller"
 
 const bootsrap = async () => {
-  const app = new App(new LoggerService())
+  const logger         = new LoggerService(),
+        userController = new UserController(logger)
+  const app = new App(logger, userController)
   await app.init()
 } 
 
